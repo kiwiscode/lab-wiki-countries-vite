057aab0e2b067fcd75b9b6dead3b5f6bd0a2fd70
Aykut Kav <ayktkav@gmail.com>
2023-11-29 20:28:10 +0100

    solved

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+// src/main.jsx
+import "bootstrap/dist/css/bootstrap.css";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import HomePage from "../src/pages/HomePage";
+import CountryDetailsPage from "../src/pages/CountryDetailsPage";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/:countryId" element={<CountryDetailsPage />} />
+    </Routes>
+  </Router>
 );
